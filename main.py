3b2389b9a815b5f63a51d5b9594c99fb2d8218f7
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-12 13:33:08 +0500

    Add _

--- main.py
+++ main.py
@@ -12,34 +12,34 @@ for i in range(3):
 
 # Dynamic Array
 # Intial list of size 3
-list = [1, 2, 3]
+list_ = [1, 2, 3]
 
 # Grow to size 4 by copying manually
 l = [0] * 4
 for i in range(3):
-    l[i] = list[i]
+    l[i] = list_[i]
 
 l[3] = 4
-list = l
+list_ = l
 
 print('\n Ouput of Dynamic array which copied manually')
 for i in range(4):
-    print(list[i])
+    print(list_[i])
 
 # Dynamic array using append method
-list = [1, 2, 3]
-list.append(4) # Time complexity is O(1)
+list_ = [1, 2, 3]
+list_.append(4) # Time complexity is O(1)
 
 print('\n Ouput of Dynamic array using append') 
-for num in list:
+for num in list_:
     print(num)
 
 # Dynamic array using inserting
-list = [1, 2, 3]
-list.insert(3, 4) # Time complexity is O(n)
+list_ = [1, 2, 3]
+list_.insert(3, 4) # Time complexity is O(n)
 
 print('\n Ouput of Dynamic array using insertion')
-for num in list:
+for num in list_:
     print(num)
 
 
