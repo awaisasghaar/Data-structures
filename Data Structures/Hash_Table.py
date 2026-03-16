1b95b8250288cc58a2a93d1535f8755b143e8c42
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-16 23:44:44 +0500

    Refactor Hash Table

--- Data Structures/Hash_Table.py
+++ Data Structures/Hash_Table.py
@@ -6,7 +6,8 @@ def hash_func(word):
     return ord(word[0].upper()) - ord('A')
 
 # Hash Table: array of lists (chaining for collisions)
-hash_table = [[] for _ in range(Hash_size)]
+hash_table = [[]for _ in range(Hash_size)]
+
 
 def insert(word):
     index = hash_func(word)
@@ -16,7 +17,19 @@ def insert(word):
 def search(word):
     index = hash_func(word)
     return word in hash_table[index]
-
+  
+def delete(word):
+    index = hash_func(word)
+    if word in hash_table[index]:
+        hash_table[index].remove(word)
+        return True
+    return False
+  
+def display():
+    for i, char in enumerate(hash_table):
+        if char:
+           letter = str(i + ord('A'))
+           print(f"Index is {i} {letter} -> {char}")
 
 # Usage
 print(insert('Awais'))
@@ -26,3 +39,7 @@ print(insert('Aspiring'))
 
 print(search('Computer Science'))
 print(search('Boys'))
+
+print('\nAfter deleting Aspiring')
+print(delete('Aspiring'))
+print(display())
