26b52724e9bf824a67c4a16eaf6cb78a52df9fb1
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-27 23:00:15 +0500

    Inserted elements in Array

--- Data Structures/Array.py
+++ Data Structures/Array.py
@@ -1,46 +1,33 @@
-# Arrays (Fixed size list)
-# fixed size list of 3 elements
-list  = [0] * 3
+# Array data structure using insert, search and delete operation
 
-list[0] = 1
-list[1] =  2
-list[2] = 3
+class Array:
+    def __init__(self):
+        self.data = []
+        self.size = 0
+    
+    # -------INSERT--------
+    def insert(self, value, index=None):
+        """Insert a value at given index"""
+        if index is None:
+            self.data.append(value)
+        elif index >= 0 and index <= self.size:
+            self.data.insert(index, value)
+        else:
+            raise IndexError(f"Index {index} out of range")
+        self.size += 1
+        return f"Value {value} inserted at index {index}"
 
-print('\n Output of fixed size array')
-for i in range(3):
-    print(list[i])
+    def display(self):
+        print(f"Array {self.data} size {self.size}")
 
-# Dynamic Array
-# Intial list of size 3
-list_ = [1, 2, 3]
 
-# Grow to size 4 by copying manually
-l = [0] * 4
-for i in range(3):
-    l[i] = list_[i]
-
-l[3] = 4
-list_ = l
-
-print('\n Ouput of Dynamic array which copied manually')
-for i in range(4):
-    print(list_[i])
-
-# Dynamic array using append method
-list_ = [1, 2, 3]
-list_.append(4) # Time complexity is O(1)
-
-print('\n Ouput of Dynamic array using append') 
-for num in list_:
-    print(num)
-
-# Dynamic array using inserting
-list_ = [1, 2, 3]
-list_.insert(3, 4) # Time complexity is O(n)
-
-print('\n Ouput of Dynamic array using insertion')
-for num in list_:
-    print(num)
+if __name__ == '__main__':
+    find = Array()
+    print(find.insert(10))
+    print(find.insert(20))
+    print(find.insert(30))
+    print(find.insert(15, index=1))
+    find.display()
 
 
 
