4f12276bfae56f7a0875c05219236ce7da31e7e7
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-28 23:01:09 +0500

    Add Binary search

--- Data Structures/Array.py
+++ Data Structures/Array.py
@@ -4,7 +4,8 @@ class Array:
     def __init__(self):
         self.data = []
         self.size = 0
-        
+
+    #  ---------- APPEND ----------  
     def append(self):
         for _ in range(3):
             num = int(input("\nNumber: "))
@@ -12,7 +13,8 @@ class Array:
             self.size += 1
             print(f"{num} is added in Array: {self.data}")
         return f"\n Array: {self.data} is size of {self.size}"
-        
+    
+    # -------- INSERT ---------
     def insert(self, index=0):
         num = int(input("\nEnter Number to insert: "))
         if index >= 0 and index <= self.size:
@@ -23,6 +25,7 @@ class Array:
         print(f"{num} inserted at index 3")
         return f"\n Array after insertion: {self.data} is size of {self.size}"
     
+    # --------- SEARCH ----------
     def search(self):
         num = int(input("\nEnter number to search: "))
         for i in range(self.size):
@@ -30,6 +33,30 @@ class Array:
                 return f"\n {num} found at index {i}"
             
         return f"\n Number not found {num} in Array: {self.data} "
+    
+    def binary_search(self):
+        num = int(input("Binary Search: "))
+
+        # Sorting Array
+        self.data.sort()
+        low = 0
+        high = self.size - 1
+
+        while low <= high:
+            # Find middle index
+            mid = (low + high) // 2
+            if self.data[mid] == num:
+                return f"\n {num} found at index {mid}"
+            
+            # Go Right
+            elif self.data[mid] < num:
+                low = mid + 1
+            # Go left
+            else:
+                high = mid - 1
+        print(f"\n {num} not found")
+
+
 
         
 if __name__ == "__main__":
@@ -37,6 +64,7 @@ if __name__ == "__main__":
    print(find.append())
    print(find.insert(index=3))
    print(find.search())
+   print(find.binary_search())
 
     # Time Complexity: O(n)
     # Space Complexity: O(1)
