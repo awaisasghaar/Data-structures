36d019523b233011095de869fa2af0478dc262ca
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-28 22:39:11 +0500

    Add search

--- Data Structures/Array.py
+++ Data Structures/Array.py
@@ -11,22 +11,32 @@ class Array:
             self.data.append(num)
             self.size += 1
             print(f"{num} is added in Array: {self.data}")
-        return f"\n Array {self.data} is size of {self.size}"
+        return f"\n Array: {self.data} is size of {self.size}"
         
     def insert(self, index=0):
-        num = int(input("\nNumber: "))
+        num = int(input("\nEnter Number to insert: "))
         if index >= 0 and index <= self.size:
             self.data.insert(index, num)
         else:
             print("Nothing happens")
         self.size += 1
         print(f"{num} inserted at index 3")
-        return f"\n Array {self.data} is size of {self.size}"
-        
+        return f"\n Array after insertion: {self.data} is size of {self.size}"
+    
+    def search(self):
+        num = int(input("\nEnter number to search: "))
+        for i in range(self.size):
+            if self.data[i] == num:
+                return f"\n {num} found at index {i}"
+            
+        return f"\n Number not found {num} in Array: {self.data} "
 
-find = Array()
-print(find.append())
-print(find.insert(index=3))
+        
+if __name__ == "__main__":
+   find = Array()
+   print(find.append())
+   print(find.insert(index=3))
+   print(find.search())
 
     # Time Complexity: O(n)
     # Space Complexity: O(1)
