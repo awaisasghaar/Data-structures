886d81965ee5be7f0894de9ad6e56493901375c7
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-03-29 22:53:41 +0500

    Add Delete function

--- Data Structures/Array.py
+++ Data Structures/Array.py
@@ -34,9 +34,9 @@ class Array:
             
         return f"\n Number not found {num} in Array: {self.data} "
     
-    # ----------Binary Search---------
+    # ---------- BINARY SEARCH ---------
     def binary_search(self):
-        num = int(input("Binary Search: "))
+        num = int(input("\nBinary Search: "))
 
         # Sorting Array
         self.data.sort()
@@ -47,16 +47,43 @@ class Array:
             # Find middle index
             mid = (low + high) // 2
             if self.data[mid] == num:
-                return f"\n {num} found at index {mid}"
+                print(f"\n {num} found at index {mid}")
+                break
             
             # Go Right
             elif self.data[mid] < num:
                 low = mid + 1
             # Go left
-            else:
+            elif self.data[mid] > num:
                 high = mid - 1
-        print(f"\n {num} not found")
+            else:
+                print(f"\n{num} not found")
+                
+        return f"\n Array after sorted: {self.data}"
+    
+    # --------- Delete by Index ---------
+    def delete_index(self, index=0):
+        if index is None:
+            print("Nothing here to delete here.")
+        if index is not None:
+            if index >= 0 and index < self.size:
+                remove = self.data.pop(index)
+                self.size -= 1
+                print(f"\nIndex {remove} removed")
+            return f"\n Array after deletion of an index: {self.data}"
+        else:
+            f"\n Invalid Index"
 
+    # ---------- Delete by Value ----------
+    def delete_value(self):
+        num = int(input("\nEnter Nunmber to delete: "))
+        if num in self.data:
+            self.data.remove(num)
+            self.size -= 1 
+            return f"\n {num} deleted Array: {self.data} size of {self.size}"
+        else:
+            print(f"\n {num} is not in the Array: {self.data}")
+    
 
 
         
@@ -66,6 +93,8 @@ if __name__ == "__main__":
    print(find.insert(index=3))
    print(find.search())
    print(find.binary_search())
+   print(find.delete_index())
+   print(find.delete_value())
 
     # Time Complexity: O(n log n)
     # Space Complexity: O(n)
