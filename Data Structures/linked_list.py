b334498aee1c7e6746f2e76839ca2fbd87d901b9
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-04-04 23:28:49 +0500

    Refactor code

--- Data Structures/linked_list.py
+++ Data Structures/linked_list.py
@@ -5,64 +5,22 @@ class Node:
         self.data = data
         self.next = None
 
-def traverse(head):
-    if head is None:
-        print("Head node is NULL")
-        return
-    current = head
-    while current is not None:
-        print(f"{current.data}", end=' ')
-        current = current.next
-    print()
+class Linkedlist:
+    def __init__(self):
+        self.head = None
 
-def insert_at_front(head, a):
-    new_node = Node(a)
-    new_node.next = head
-    return new_node
+        # ------- INSERT OPERATION -------
+        def insert_at_beginning(self, data):
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+            print(f"\n Insert data at the beginning")
 
-def search_num(head, num):
-    current = head
-    while current is not None:
-        if current.data == num:
-            return True
-        current = current.next
-    return False
 
-def insert_at_end(head, x):
-    new_node = Node(x)
-    if head is None:
-        return new_node
-    last = head
-    while last.next is not None:
-        last = last.next
-    last.next = new_node
-    return head
-
-def print_list(head):
-    current = head
-    while current is not None:
-        if current.next is not None:
-            print(f"{current.data} -> ", end="")
-        else:
-            print(current.data)
-        current = current.next
 
 if __name__ == "__main__":
-    head = Node(5)
-    head.next = Node(10)
-    head.next.next = Node(15)
-    head.next.next.next = Node(20)
-
-    traverse(head)               # 5 10 15 20
-
-    head = insert_at_front(head, 1)
-    print_list(head)             # 1 -> 5 -> 10 -> 15 -> 20
-
-    print(search_num(head, 15))  # True
-    print(search_num(head, 99))  # False
-
-    head = insert_at_end(head, 92)
-    print_list(head)             # 1 -> 5 -> 10 -> 15 -> 20 -> 92
+    main = Linkedlist
+    main.insert_at_beginning()
 
 
     
