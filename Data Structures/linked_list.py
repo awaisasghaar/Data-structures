3f823aa57d5a46fe4e8651142b3fe0378b8b267d
Awais Asghar <139065109+awaisasghaar@users.noreply.github.com>
2026-04-07 22:38:24 +0500

    Add Delete operation

--- Data Structures/linked_list.py
+++ Data Structures/linked_list.py
@@ -16,7 +16,6 @@ class LinkedList:
             print(current.data, end=" -> ")
             current = current.next
         return
-    print("None")
 
     # ------ INSERT AT HEAD ------
     def insert_at_head(self, data):
@@ -34,7 +33,8 @@ class LinkedList:
         while current.next:
             current = current.next
         current.next = node
-    
+
+    # ------- INSERT AFTER PREVIOUS VALUE ---------
     def insert_after_value(self, target, data):
         current = self.head
         while current:
@@ -45,7 +45,21 @@ class LinkedList:
                 return True
             current = current.next
         return False
-
+    
+    # -------- DELETE A NODE BY VALUE --------
+    def delete(self, data):
+        if not self.head:
+            return False
+        if self.head.data == data:
+            self.head == self.head.next
+            return True
+        prev, current = self.head, self.head.next
+        while current:
+            if current.data == data:
+                prev.next = current.next
+                return True
+            prev, current = current, current.next
+        return False
 
         
 
@@ -54,6 +68,8 @@ if __name__ == "__main__":
     l.insert_at_head(43)
     l.insert_at_tail(42)
     l.insert_after_value(42, 20)
+    l.insert_after_value(20, 30)
+    l.delete(20)
     l.traverse()
 
 
